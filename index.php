c91b539bfa70b9907f76684829ea862c5700490f
MatteoArt <mattrim8@gmail.com>
2023-09-08 17:32:21 +0200

    inizio esercizio

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="lunghezza">Lunghezza password:</label>
+        <input type="number" id="lunghezza" name="lung">
+    </form>
+</body>
+</html>
